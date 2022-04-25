10704a513e8195ef8b64dc39181ac6ee60e7bd1e
Kentrell Saxby <102038810+Kentrell48@users.noreply.github.com>
2022-04-25 13:35:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function ChangeColor(){
+  document.body.style.backgroundColor=genRandomColor();
+  document.getElementById("r").style.color=genRandomColor();
+}
+
+function genRandomColor(){
+  let r=0,g=0,b=0;
+  r=Math.round(Math.random()*255);
+  g=Math.round(Math.random()*255);
+  b=Math.round(Math.random()*255);
+  console.log("rgb("+r+","+g+","+b+")")
+    return "rgb("+r+","+g+","+b+")";
+
+
+  
+}
